f1ba4192baa0539a58a1fed31bfffa6c5c6b5035
Miguel.Ferro <miguelferro@LPT-4JPQ5J3.localdomain>
2022-12-17 14:27:00 +0000

    simplifying template

--- template/a.py
+++ template/a.py
@@ -1,8 +1,3 @@
-def run():
+f = open('input.txt', 'r').read().strip().split('\n')
+# f = open('test.txt', 'r').read().strip().split('\n')
 
-    #f = open('input.txt', 'r').read().strip().split('\n')
-    f = open('test.txt', 'r').read().strip().split('\n')
-
-
-if __name__ == '__main__':
-    run()
