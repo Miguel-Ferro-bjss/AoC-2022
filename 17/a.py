6caf7ed6655ff384023ab9acab483739f7b5678f
Miguel.Ferro <miguelferro@LPT-4JPQ5J3.localdomain>
2022-12-17 21:32:08 +0000

    day 17 - part 2

--- 17/a.py
+++ 17/a.py
@@ -41,22 +41,32 @@ def draw():
     for g in grid[::-1]:
         print(" ".join(g))
 
-s_i = 0
-for i in range(2022):
 
+def stack_signature():
+    # hyper-neutrino to the rescue!
+    height_map = [-20] * 7
+    for r in rock_stack:
+        height_map[r[0]] = max(height_map[r[0]], r[1])
+    top = max(height_map)
+    return tuple(y - top for y in height_map)
+
+s_i = 0
+L = 1000000000000 # part 2
+# L = 2022 # part 1
+i = 0
+seen = {}
+while i < L:
     key = keys[i % len(keys)]
     rock = rocks[key]
+    h = highest_rock()
 
-    highest = highest_rock()
     if key != '+':
-        rock = move_rock(rock, (2, highest + 4)) # initialize rock
+        rock = move_rock(rock, (2, h + 4)) # initialize rock
     else:
-        rock = move_rock(rock, (3, highest + 4))
+        rock = move_rock(rock, (3, h + 4))
 
     while True:
         m = moves[stream[s_i % len(stream)]] #stream move (x,y)
-        # print(rock)
-        # print(m)
         s_i += 1
         new_rock = move_rock(rock, m)
         if possible_pos(new_rock):
@@ -70,8 +80,20 @@ for i in range(2022):
         else:
             # print(rock)
             stack_rock(rock)
+            h = highest_rock()
+            seen_key = (s_i % len(stream), key, stack_signature())
+            if seen_key in seen:
+                # neat trick (credits to hyper-neutrino)
+                prev_i, prev_h = seen[seen_key] 
+                rocks_remaining = L - i
+                rep_remaining = rocks_remaining // (i - prev_i)
+                h_offset = rep_remaining * (h - prev_h)
+                i += rep_remaining * (i - prev_i)
+                seen = {}
+                if i >= L:
+                    break
+            seen[seen_key] = (i, h)
             break
-    # print(rock_stack)
-
+    i += 1
 # draw()
-print(highest_rock() + 1)
+print(h + 1 + h_offset)
