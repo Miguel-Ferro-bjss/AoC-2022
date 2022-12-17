f703056b0cf9849797db77a1d280e069e124456a
Miguel.Ferro <miguelferro@LPT-4JPQ5J3.localdomain>
2022-12-17 00:59:50 +0000

    day 16 - finally done!

--- 16/a.py
+++ 16/a.py
@@ -1,9 +1,16 @@
 from collections import deque
+from itertools import chain, combinations
+
+"""
+A lot of this was 'stolen' from hyperneutrino's walkthrough on Youtube.
+I didn't have the courage to implement the caching mechanism with the bitmask,
+so I just waited and used my raging CPU fan as a space heater :)
+"""
 
 def parse_input():
 
-    # f = open('input.txt', 'r').read().strip().split('\n')
-    f = open('test.txt', 'r').read().strip().split('\n')
+    f = open('input.txt', 'r').read().strip().split('\n')
+    # f = open('test.txt', 'r').read().strip().split('\n')
 
     data = {}
 
@@ -51,38 +58,38 @@ for valve in data:
 
 # print(time_to)
 
-def optimum_steps(time, valve, opened, allowed_valves):
-
-    # checker = "".join(sorted(opened))
-    # if (time, valve, checker) in tried:
-    #     return tried[(time, valve, checker)]
+def optimum_steps(time, valve, opened):
 
     max_release = 0
     for adjacent in time_to[valve]:
-        if adjacent in opened or adjacent not in allowed_valves:
+        if adjacent in opened:
             continue # no point in oppening an open valve
         time_remaining = time - time_to[valve][adjacent] - 1 # we open the valve
         if time_remaining < 1: # no time to get any flow
             continue # nothing possible to do
         op = opened.copy()
         op.append(adjacent)
-        max_release = max(max_release, optimum_steps(time_remaining, adjacent, op, time_to) + data[adjacent]['flow'] * time_remaining)
-    
-    # tried[(time, valve, checker)] = max_release
-
+        max_release = max(max_release, optimum_steps(time_remaining, adjacent, op) + data[adjacent]['flow'] * time_remaining)
+  
     return max_release
 
-allowed_valves = flow_valves # all valves allowed - restriction only for part 2
-print(optimum_steps(30, 'AA', [], allowed_valves))
+print(optimum_steps(30, 'AA', []))
 
 # number of ways in which we can split the existing valves between 2 workers
-# (only need to check half because the workers are indistiguishable)
-partitions = len(time_to)//2
+
+def powerset(iterable):
+    "powerset([1,2,3]) --> () (1,) (2,) (3,) (1,2) (1,3) (2,3) (1,2,3)"
+    s = list(iterable)
+    return list(chain.from_iterable(combinations(s, r) for r in range(len(s)+1)))
+
+partitions = powerset(flow_valves)
+partitions = [x for x in partitions if len(x) != 0 and len(x) != len(flow_valves)]
+
 max_release = 0
-for i in range(1, partitions):
-    my_valves = flow_valves[0:i]
-    el_valves = flow_valves[i::]
-    
-    max_release = max(max_release, optimum_steps(26, 'AA', [], my_valves) + optimum_steps(26, 'AA', [], el_valves))
+for p in partitions:
+    my_valves = list(p)
+    el_valves = [x for x in flow_valves if x not in my_valves]
+    # print(my_valves, el_valves)
+    max_release = max(max_release, optimum_steps(26, 'AA', my_valves) + optimum_steps(26, 'AA', el_valves))
 
 print(max_release)
